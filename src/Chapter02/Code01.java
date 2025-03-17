60b902ff3b84aa5e98494e8866176110f23a0299
ysjang0706 <ysjang706@gmail.com>
2025-03-17 12:03:34 +0900

    main Method 사용법, 연결연산자, 덧셈연산자, println print 차이점

--- src/Chapter02/Code01.java
+++ src/Chapter02/Code01.java
@@ -7,7 +7,7 @@ public class Code01 {
         System.out.printf("Grade 1\n");
         System.out.println("result : "+100+200);
         System.out.println("result : "+(100+200));
-        System.out.println(100 + 200 + "won");
+        System.out.println(100 + 200 + "wo");
     }
 
 }
