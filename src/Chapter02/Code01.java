cc9f4ff622383f6cca8ffde8cb428fad8adacd66
ysjang0706 <ysjang706@gmail.com>
2025-03-17 11:52:33 +0900

    main Method 사용법, 연결연산자, 덧셈연산자, println print 차이점

---
+++ src/Chapter02/Code01.java
@@ -0,0 +1,13 @@
+package Chapter02;
+
+public class Code01 {
+    public static void main(String[] args) {
+        System.out.println("AISoftware");
+        System.out.print("YOONSUNG JANG\n");
+        System.out.printf("Grade 1\n");
+        System.out.println("result : "+100+200);
+        System.out.println("result : "+(100+200));
+        System.out.println(100 + 200 + "won");
+    }
+
+}
