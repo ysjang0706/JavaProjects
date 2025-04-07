08b77de0b81ac4bcef3c892b926c11747a8f16ab
ysjang0706 <ysjang706@gmail.com>
2025-04-07 11:23:49 +0900

    증감 연산자

---
+++ src/Chapter03/Code3Lab02.java
@@ -0,0 +1,15 @@
+package Chapter03;
+
+public class Code3Lab02 {
+    public static void main(String[] args) {
+        int total = 0;
+        total -= 10*900;
+        total += 2*1800;
+        total -= 5*3500;
+        total += 4*4000;
+        total += 1500;
+        total += 4*2000;
+        total += 5*1800;
+        System.out.printf("오늘 총 매출액은 %d원입니다.",total);
+    }
+}
