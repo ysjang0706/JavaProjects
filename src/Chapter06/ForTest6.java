b1386fa2b27bd2958d4bce35a131929e012f5359
ysjang0706 <ysjang706@gmail.com>
2025-05-12 10:51:34 +0900

    6장 반복문 - For6, 중첩 for문

---
+++ src/Chapter06/ForTest6.java
@@ -0,0 +1,11 @@
+package Chapter06;
+
+public class ForTest6 {
+    public static void main(String[] args) {
+        for(int i = 0; i <= 2; i++){
+            for(int j = 0; j <= 1; j++){
+                System.out.printf("인공지능소프트웨어 자바 알고리즘 실습 수업: (i : %d , j : %d)\n",i,j);
+            }
+        }
+    }
+}
