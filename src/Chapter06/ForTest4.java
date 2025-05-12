338aa2a81e927ac06552fce39aa5cc531041e258
ysjang0706 <ysjang706@gmail.com>
2025-05-12 10:34:59 +0900

    6장 반복문 - For4, 짝수의 합계

--- src/Chapter06/ForTest4.java
+++ src/Chapter06/ForTest4.java
@@ -4,7 +4,9 @@ public class ForTest4 {
     public static void main(String[] args) {
         int n = 0;
         int count = 10;
-        for(int i = 1; i <= 10; i++) n += i;
-        System.out.printf("1부터 %d까지의 합은 %d입니다.",count,n);
+        for(int i = 1; i <= 10; i++){
+            if(i%2==0) n += i;
+        }
+        System.out.printf("1부터 %d까지 짝수들의 합은 %d입니다.",count,n);
     }
 }
